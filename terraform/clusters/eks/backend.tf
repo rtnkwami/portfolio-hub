6414d79544212eaf9bfe578b950f573b21450dde
rtnkwami <nukukwami@outlook.com>
2026-06-16 21:51:46 +0000

    chore(terraform/eks): separate provider and backend code into different files

--- terraform/clusters/eks/backend.tf
+++ terraform/clusters/eks/backend.tf
@@ -1,16 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.40.0"
-    }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "3.1.1"
-    }
-  }
-
   backend "s3" {
     bucket       = "niovial-opentofu-state"
     key          = "portfolio-eks-tfstate"
---
+++ terraform/clusters/eks/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.40.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.1.1"
+    }
+  }
+}
