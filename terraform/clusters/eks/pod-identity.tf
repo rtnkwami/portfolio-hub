ca0badd02fe230fca6b3d4cdc6fcc822bdc82f79
rtnkwami <nukukwami@outlook.com>
2026-06-17 08:39:12 +0000

    chore(terraform/eks): move pod identity configs to dedicated file

---
+++ terraform/clusters/eks/pod-identity.tf
@@ -0,0 +1,27 @@
+module "karpenter" {
+  source                        = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version                       = local.versions.modules.eks
+  cluster_name                  = module.eks.cluster_name
+  node_iam_role_name            = "karpenter-node-role-${module.eks.cluster_name}"
+  node_iam_role_use_name_prefix = false
+  enable_spot_termination       = true
+  queue_name                    = "karpenter-interruption-queue-${module.eks.cluster_name}"
+
+  tags = merge(local.global_tags)
+}
+
+module "aws_ebs_csi_pod_identity" {
+  source = "terraform-aws-modules/eks-pod-identity/aws"
+
+  name                      = "aws-ebs-csi"
+  attach_aws_ebs_csi_policy = true
+
+  associations = {
+    this = {
+      cluster_name    = module.eks.cluster_name
+      namespace       = "kube-system"
+      service_account = "ebs-csi-controller-sa"
+    }
+  }
+  tags = merge(local.global_tags)
+}
