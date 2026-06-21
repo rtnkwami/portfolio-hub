7b7065a0730fb7595685515efc528ca9721d2cee
rtnkwami <nukukwami@outlook.com>
2026-06-21 18:34:30 +0000

    feat(terraform/hcloud): add load balancer in front of control plane nodes

--- terraform/clusters/hcloud/main.tf
+++ terraform/clusters/hcloud/main.tf
@@ -63,6 +63,41 @@ resource "hcloud_server" "control_plane" {
   }
 }
 
+resource "hcloud_load_balancer" "controlplane_lb" {
+  name = "controlplane-lb"
+  load_balancer_type = "lb11"
+  network_zone = "eu-central"
+}
+
+# attach lb to network
+resource "hcloud_load_balancer_network" "cp_lb_attachment" {
+  load_balancer_id = hcloud_load_balancer.controlplane_lb.id
+  network_id = hcloud_network.private_network.id
+}
+
+resource "hcloud_load_balancer_service" "cp__lb_listener" {
+  load_balancer_id = hcloud_load_balancer.controlplane_lb.id
+  protocol = "tcp"
+  # kube-apiserver listens on 6443
+  listen_port = 6443
+  destination_port = 6443
+
+  # verify that kube-apiserver can be reached
+  health_check {
+    protocol = "tcp"
+    port = 6443
+    retries = 3
+    interval = 10
+    timeout = 5
+  }
+}
+
+resource "hcloud_load_balancer_target" "cp_lb_target" {
+  type = "label_selector"
+  load_balancer_id = hcloud_load_balancer.controlplane_lb.id
+  label_selector = "niovial.io/node-purpose=control-plane"
+}
+
 # resource "hcloud_firewall" "firewall" {
 #   name = "${var.project_name}-firewall"
 
