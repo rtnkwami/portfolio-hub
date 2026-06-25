c9b581da2d23cc2d29f15213a91568166a49b785
rtnkwami <nukukwami@outlook.com>
2026-06-25 16:49:31 +0000

    chore(terraform/hcloud): move nodepools out of cluster autoscaler file into nodepool file

--- terraform/clusters/hcloud/cluster_autoscaler.tf
+++ terraform/clusters/hcloud/cluster_autoscaler.tf
@@ -1,58 +1,5 @@
 locals {
   zones = ["nbg1", "fsn1", "hel1"]
-
-  nodepool_config = {
-    system = {
-      sizes = {
-        small = "cx23"
-        medium = "cx33" 
-      }
-      min = 0
-      max = 3
-      labels = {
-        "niovial.io/node-purpose" = "system"
-      }
-      taints = [
-        { 
-          key = "niovial.io/node-purpose"
-          value = "system"
-          effect = "NoSchedule"
-        }
-      ]
-    }
-    general = {
-      sizes = {
-        small  = "cx23"
-        medium = "cx33"
-        large  = "cx43"
-        xlarge = "cx53"
-      }
-      min    = 0
-      max    = 20
-      labels = {
-        "niovial.io/node-purpose" = "general"
-      }
-      taints = []
-    }
-    database = {
-      sizes = {
-        medium = "ccx23"
-        large  = "ccx33"
-      }
-      min    = 0
-      max    = 10
-      labels = {
-        "niovial.io/node-purpose" = "database"
-      }
-      taints = [
-        { 
-          key = "niovial.io/node-purpose"
-          value = "database"
-          effect = "NoSchedule"
-        }
-      ]
-    }
-  }
   # create a nodepool per intent, instance type, and zone
   # this is meant to satisfy topology spread constraints such as
   # topology.kubernetes.io/zone and hostname, among others
