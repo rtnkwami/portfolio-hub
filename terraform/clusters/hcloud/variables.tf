bf8c427b7427e68fcdabd4fa41a2ea6fcf1c5317
rtnkwami <nukukwami@outlook.com>
2026-08-03 19:31:35 +0000

    chore: clean up unused variables

--- terraform/clusters/hcloud/variables.tf
+++ terraform/clusters/hcloud/variables.tf
@@ -18,11 +18,3 @@ variable "infisical_client_id" {
 variable "infisical_client_secret" {
   sensitive = true
 }
-
-variable "tailscale_client_id" {
-  sensitive = true
-}
-
-variable "tailscale_client_secret" {
-  sensitive = true
-}
